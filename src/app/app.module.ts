80144796fe9b9e87b807e96c47f4724e3742dbee
Vladislav <vlad.sadretdinov@gmail.com>
2018-07-14 16:32:36 +0300

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from "@ngrx/store";
+import  { reducer} from "./reducers/city.reducer";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      city: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
